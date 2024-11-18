d4fc4b138b76a5e8580adf84939a1689fe99c7c0
Eli Baird <ecb8660@uncw.edu>
2024-11-17 23:20:07 -0500

    added add function

--- math_ops.py
+++ math_ops.py
@@ -4,8 +4,13 @@ def divide_numbers(a, b):
     """Divides two numbers and returns the result."""
     return a / b  # Potential division by zero error
 
+def add(a,b):
+    return a+b
+
 if __name__ == "__main__":
     x = 10
     y = 0
     result = divide_numbers(x, y)
+    result2 = add(3,4)
     print(f"The result of division is: {result}")
+    print(f"The result of additionis: {result2}")
